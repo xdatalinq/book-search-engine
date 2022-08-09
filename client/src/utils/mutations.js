3a941f9557f41a9516add5d78b1eb7d04551cebf
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-09 00:54:24 -0400

    instructions from project page, coming back to this file later

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+
+
+
+mutations.js:
+
+    LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+
+    ADD_USER will execute the addUser mutation.
+
+    SAVE_BOOK will execute the saveBook mutation.
+
+    REMOVE_BOOK will execute the removeBook mutation.
+
