40880f8f69703af03dc943c1a73387b94413f846
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-09 00:39:08 -0400

    queries.js update: using the same structure from typeDefs on the server side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,20 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
