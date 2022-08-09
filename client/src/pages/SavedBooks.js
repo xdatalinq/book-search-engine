e5fe6feb26f06fdf25cb8d49798d9783f22df93e
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-09 12:35:24 -0400

    switched useQuery in savedBooks and added useMutation for remove_book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
+import { useQuery, useMutation } from "@apollo/client";
 import { getMe, deleteBook } from '../utils/API';
+import { GET_ME } from "../utils/queries";
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
-
+  const [userData, setUserData] = useQuery({GET_ME});
+  const [removeBook] = useMutation(REMOVE_BOOK);
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
