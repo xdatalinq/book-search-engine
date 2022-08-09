e2d4d1022e12877673b63c7c4d58428fd865bddf
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-08 22:29:04 -0400

    typeDefs rough cleanup

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,20 +10,13 @@ const typeDefs = gql`
     friends: [User]
   }
 
-  type Thought {
+  type Book {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
     reactions: [Reaction]
-  }
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
   }
 
   type Auth {
@@ -34,18 +27,18 @@ const typeDefs = gql`
   type Query {
     me: User
     users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
+    
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addReaction(thoughtId: ID!, reactionBody: String!): Thought
-    addFriend(friendId: ID!): User
+    
   }
-`;
+
+`; 
+
+// need to add saved books and remove books mutations here, need to update book model
+// user model also
 
 module.exports = typeDefs;
