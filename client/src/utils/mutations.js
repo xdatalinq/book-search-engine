536dfcebf13e79b4b8e672ccbbee2cc5c7137400
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-09 02:08:45 -0400

    found actual mutation syntax for login + add_user in 21.2.3

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,19 +1,25 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql
-
-
-export const ADD_USER = gql
-
-
-export const SAVE_BOOK = gql
-mutation {
-    addPost(title: "GraphQL Tutorial", body: "Let's learn GraphQL by first installing...") {
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       _id
-      title
-      body
+      username
+      email
     }
   }
+
+export const ADD_USER = gql
+mutation {
+    addUser(username:"tester", password:"test12345", email:"test@test.com") {
+      _id
+      username
+      email
+    }
+  }
+
+export const SAVE_BOOK = gql
+
 
 export const REMOVE_BOOK = gql    
     
