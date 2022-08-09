fe68ea0e4db3fb85df6b282a896064df604d3263
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-09 14:54:51 -0400

    I had forgottent to import useMutation..

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,6 +3,7 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { ADD_USER } from "../utils/mutations";
 // import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
 
 const SignupForm = () => {
   // set initial form state
