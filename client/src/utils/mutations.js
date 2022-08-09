b70f761a7992ae8fe20bb0b15d03af2402919fab
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-09 01:51:31 -0400

    found starter syntax from module

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,20 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql
-    LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+
 
 export const ADD_USER = gql
-    ADD_USER will execute the addUser mutation.
+
 
 export const SAVE_BOOK = gql
-    SAVE_BOOK will execute the saveBook mutation.
+mutation {
+    addPost(title: "GraphQL Tutorial", body: "Let's learn GraphQL by first installing...") {
+      _id
+      title
+      body
+    }
+  }
 
 export const REMOVE_BOOK = gql    
-    REMOVE_BOOK will execute the removeBook mutation.
+    
 
