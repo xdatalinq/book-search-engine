888fbcd8e27c1b05184a727cbb2410c58ad0f328
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-09 01:15:59 -0400

    need to figure out syntax..

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,15 +1,14 @@
 import { gql } from '@apollo/client';
 
-
-
-
-mutations.js:
-
+export const LOGIN_USER = gql
     LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
 
+export const ADD_USER = gql
     ADD_USER will execute the addUser mutation.
 
+export const SAVE_BOOK = gql
     SAVE_BOOK will execute the saveBook mutation.
 
+export const REMOVE_BOOK = gql    
     REMOVE_BOOK will execute the removeBook mutation.
 
